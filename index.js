20efe4c4b376b78e695d410af5a5feff30c31170
Gurneet-08 <136553291+Gurneet-08@users.noreply.github.com>
2026-02-26 17:29:51 +0530

    initial

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const axios = require("axios");
 const generatePost = require("./generatePost");
 
 (async () => {
-  const post = await generatePost();
-  console.log(post);
-})();
+  try {
+    const post = await generatePost();
 
+    console.log("Generated Post:\n", post);
+
+    // Send to Make webhook
+    await axios.post("https://hook.eu1.make.com/qbpo96hmx09mo2otq8cl5v1mu3x7ljml", {
+      post: post
+    });
+
+    console.log("Post sent to Make successfully!");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+})();
